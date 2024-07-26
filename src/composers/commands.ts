e4da06176ac3e2b1e0884d3c6535ed9d20611a36
Nikita Koshelenko <nikitakoschelenko@gmail.com>
2024-07-26 19:03:56 +0300

    fix: возможность вызывать бота только в указанном чате

--- src/composers/commands.ts
+++ src/composers/commands.ts
@@ -1,33 +1,35 @@
 import { Composer } from 'grammy'
 
 import { getCompetitionEntrants } from '../api'
-import { COMPETITIONS, PERSONAL_CODE } from '../config'
+import { COMPETITIONS, PERSONAL_CODE, TELEGRAM_CHAT_ID } from '../config'
 import { mapCompetitionEntrantToText, mapCompetitionToText } from '../mappers'
 import { prisma } from '../prisma'
 
 export const commandsComposer = new Composer()
 
-commandsComposer.command('start', async (ctx) => {
-  for (const competitionId of COMPETITIONS) {
-    const data = await getCompetitionEntrants(competitionId)
-    const competition = data[0]
+commandsComposer
+  .filter((ctx) => ctx.chatId === TELEGRAM_CHAT_ID)
+  .command('start', async (ctx) => {
+    for (const competitionId of COMPETITIONS) {
+      const data = await getCompetitionEntrants(competitionId)
+      const competition = data[0]
 
-    const text = [
-      mapCompetitionToText(competition),
-      mapCompetitionEntrantToText(competition, PERSONAL_CODE),
-    ].join('\n\n')
+      const text = [
+        mapCompetitionToText(competition),
+        mapCompetitionEntrantToText(competition, PERSONAL_CODE),
+      ].join('\n\n')
 
-    await ctx.reply(text, {
-      parse_mode: 'Markdown',
-    })
+      await ctx.reply(text, {
+        parse_mode: 'Markdown',
+      })
 
-    await prisma.competition.upsert({
-      where: { id: competition.id },
-      create: {
-        id: competition.id,
-        updatedAt: new Date(competition.updated_at),
-      },
-      update: { updatedAt: new Date(competition.updated_at) },
-    })
-  }
-})
+      await prisma.competition.upsert({
+        where: { id: competition.id },
+        create: {
+          id: competition.id,
+          updatedAt: new Date(competition.updated_at),
+        },
+        update: { updatedAt: new Date(competition.updated_at) },
+      })
+    }
+  })
